4c0a0087c0a06993cfd9eadf12440bc1921f601f
AFutureD <f20140807@outlook.com>
2018-07-06 22:58:29 +0800

    code 1008 1009

--- 1008/main.cpp
+++ 1008/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <stack>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define REP(i,n) for(int i = 0; i < (n); i++)
+#define REP_1(i,n) for(int i = 1; i < (n); i++)
+#define FOR(i,A,B) for(int i = (A); i < (B); i++)
+#define REP_N(i,n) for(int i = 0; i <= (n); i++)
+#define REP_1_N(i,n) for(int i = 1; i <= (n); i++)
+#define FOR_N(i,A,B) for(int i = A; i <= (B); i++)
+#define DWN(i,n) for(int i = (n); i > 0; i--)
+#define DWN_1(i,n) for(int i = (n); i > 1; i--)
+#define FOR_D(i,A,B) for(int i = (A); i > (B); i--)
+#define DWN_N(i,n) for(int i = (n); i >= 0; i--)
+#define DWN_1_N(i,n) for(int i = (n); i >= 1; i--)
+#define FOR_D_N(i,A,B) for(int i = (A); i >= (B); i--)
+
+using namespace std;
+
+int n;
+
+int main(){
+
+    cin >>n;
+    int last = 0;
+    int ans = 0;
+    while(n--){
+//        cout << "N: " << n <<endl;
+        int point;
+        cin >> point;
+        int count = point - last;
+        ans += (count >= 0 ) ? count * 6 : (-count) * 4;
+//        cout << "One: " << count << " " << ans <<endl;
+        ans += 5;
+//        cout << "Two: " << ans <<endl;
+        last = point;
+    }
+    cout << ans <<endl;
+	return 0;
 }
---
+++ 1009/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <cstring>
+#include <map>
+#include <stack>
+
+#define REP(i,n) for(int i = 0; i < (n); i++)
+#define REP_1(i,n) for(int i = 1; i < (n); i++)
+#define FOR(i,A,B) for(int i = (A); i < (B); i++)
+#define REP_N(i,n) for(int i = 0; i <= (n); i++)
+#define REP_1_N(i,n) for(int i = 1; i <= (n); i++)
+#define FOR_N(i,A,B) for(int i = A; i <= (B); i++)
+#define DWN(i,n) for(int i = (n); i > 0; i--)
+#define DWN_1(i,n) for(int i = (n); i > 1; i--)
+#define FOR_D(i,A,B) for(int i = (A); i > (B); i--)
+#define DWN_N(i,n) for(int i = (n); i >= 0; i--)
+#define DWN_1_N(i,n) for(int i = (n); i >= 1; i--)
+#define FOR_D_N(i,A,B) for(int i = (A); i >= (B); i--)
+
+using namespace std;
+
+
+int n,m;
+map<int,float> a,b,ans;
+
+bool cmp(const pair<int,float>& x, const pair<int,float>& y){
+    return x.first>y.first;
+}
+
+int main(){
+    freopen("a.txt","r",stdin);
+
+    cin >> n;
+    REP(i,n){
+        int tmpa = 0; float tmpb = 0;
+//        cin >> tmpa >> tmpb;
+        scanf("%d%f",&tmpa,&tmpb);
+        a[tmpa] = tmpb;
+    }
+
+    cin >> m;
+    REP(i,m){
+        int tmpa = 0; float tmpb = 0;
+//        cin >> tmpa >> tmpb;
+        scanf("%d%f",&tmpa,&tmpb);
+        b[tmpa] = tmpb;
+    }
+
+    for(map<int,float>::iterator i = a.begin(); i != a.end();i++){
+        for(map<int,float>::iterator j = b.begin(); j != b.end();j++){
+            int num = i->first + j->first;
+            float count = i->second * j->second;
+            map<int,float>::iterator ser = ans.find(num);
+            if(ser == ans.end()){
+                ans[num] = count;
+            } else{
+                ser->second += count;
+            }
+        }
+    }
+    vector < pair<int,float> > tmpAns(ans.begin(),ans.end() );
+    sort(tmpAns.begin(),tmpAns.end(),cmp);
+
+    int pl = 0;
+    for(int i = 0;i < tmpAns.size();i++){
+        if(tmpAns[i].second != 0.0)
+            pl++;
+    }
+
+    cout << pl;
+    for(int i = 0;i < tmpAns.size();i++){
+        if(tmpAns[i].second != 0.0)
+            printf(" %d %.1f",tmpAns[i].first,tmpAns[i].second);
+    }
+
+    return 0;
+}
